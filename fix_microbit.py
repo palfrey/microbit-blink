c40f90324111da6ec3d89fd86715093759725e4f
Tom Parker <palfrey@tevp.net>
2016-07-24 15:34:17 +0100

    Actually find proper createBLEInstance code

--- fix_microbit.py
+++ fix_microbit.py
@@ -16,9 +16,3 @@ i2cpp_file = "lib/microbit-dal/source/drivers/MicroBitI2C.cpp"
 i2cpp = open(i2cpp_file).read()
 i2cpp = i2cpp.replace("twi_master_init_and_clear()", "twi_master_init_and_clear(NRF_TWI1)")
 open(i2cpp_file, "w").write(i2cpp)
-
-# FIXME: Horrible workaround for createBLEInstance not being found anywhere
-blecpp_file = "lib/BLE_API/source/BLE.cpp"
-blecpp = open(blecpp_file).read()
-blecpp = blecpp.replace("#ifndef YOTTA_CFG_BLE_INSTANCES_COUNT\n    INITIALIZER_LIST_FOR_INSTANCE_CONSTRUCTORS", "#ifndef YOTTA_CFG_BLE_INSTANCES_COUNT\n    //INITIALIZER_LIST_FOR_INSTANCE_CONSTRUCTORS")
-open(blecpp_file, "w").write(blecpp)
